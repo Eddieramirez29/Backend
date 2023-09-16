0895c3e00347b883d30d8892960713c316a21a92
Eduardo <tkd13000@gmail.com>
2023-09-16 11:36:30 -0600

    Cambio de puerto, del 3001 al 27027

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import {} from "dotenv/config"
 import { jsonParser } from "./src/middleware/bodyParser"
 import registerRouter from "./src/routers/registerRouter"
 import { DBConn } from "./src/config/database"
+const cors = require('cors');
 
 
 const app = express()
-const port = 3001
+const port = 27027
 
 DBConn()
 
 // Middleware
 app.use(jsonParser)
+
+// Habilita CORS para todos los orígenes (en desarrollo)
+app.use(cors());
 
 // Routers
 app.use(
