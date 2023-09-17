5536b85897c9dbb9b48f7df7bbfda2aabc13c3be
Eduardo <tkd13000@gmail.com>
2023-09-16 22:00:17 -0600

    Se agrega la validación de la existencia o no de usuarios en la DB

--- src/controllers/getUserController.js
+++ src/controllers/getUserController.js
@@ -1,10 +1,23 @@
- import User from "../models/User"
+import User from "../models/User";
 
 export const getUser = async (_, response) => {
-    const users = await User.find({});//Extra la información de todos los usuarios
+  try {
+    const users = await User.find({}); // Extrae la información de todos los usuarios
 
-    response.send(users);
-}
+    if (users.length === 0) {
+      // Verifica si no se encontraron usuarios
+      response.status(404).send("No existe el registro en la base de datos");
+    } else {
+      // Envía la lista de usuarios si se encontraron usuarios
+      response.status(200).send(users);
+    }
+  } catch (error) {
+    // Maneja errores si ocurren al consultar la base de datos
+    console.error("Error al obtener usuarios:", error);
+    response.status(500).send("Error interno del servidor");
+  }
+};
+
 
 export const getUserById = async (request, response) => {
     const { id } = request.params
